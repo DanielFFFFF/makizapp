82e2623688a9a9eb32d36900c4dda796c80f5c73
HanaeMelehi <melehi01hanae@gmail.com>
2024-11-18 00:38:26 +0100

    fixed admin role ungranted permissions

--- SpringBootServer/src/main/java/fr/makizart/restserver/CustomUserDetailsService.java
+++ SpringBootServer/src/main/java/fr/makizart/restserver/CustomUserDetailsService.java
@@ -8,6 +8,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -20,6 +26,24 @@ public class CustomUserDetailsService implements UserDetailsService {
         if (utilisateur == null) {
             throw new UsernameNotFoundException("User not found");
         }
-        return new org.springframework.security.core.userdetails.User(utilisateur.getUsername(), utilisateur.getPassword(), utilisateur.getAuthorities());
+
+        // Initialisation de la liste des autorités
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        // Ajoute l'autorité ROLE_ADMIN si l'utilisateur a le rôle ADMIN
+        if ("ADMIN".equalsIgnoreCase(utilisateur.getRole())) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+
+        // Convertit les autorités existantes en SimpleGrantedAuthority, si elles existent
+        authorities.addAll(utilisateur.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getAuthority()))
+                .collect(Collectors.toList()));
+
+        return new org.springframework.security.core.userdetails.User(
+                utilisateur.getUsername(),
+                utilisateur.getPassword(),
+                authorities
+        );
     }
 }
